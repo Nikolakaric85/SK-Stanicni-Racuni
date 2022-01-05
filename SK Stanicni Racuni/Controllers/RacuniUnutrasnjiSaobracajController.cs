f7bee021fb1eb83db09821b6eed20dd813452a21
Nikola Karić <nikolakaricsd@gmail.com>
2022-01-05 16:06:25 +0100

    Add project files.

---
+++ SK Stanicni Racuni/Controllers/RacuniUnutrasnjiSaobracajController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SK_Stanicni_Racuni.Controllers
+{
+    public class RacuniUnutrasnjiSaobracajController : Controller
+    {
+
+        public ActionResult ListaStanica(string stanica)
+        {
+            List<string> lista = new List<string> { "Smederevo", "Beograd", "Niš", "Kragujevac", "Novi Sad", "Kraljevo" };
+
+            return Json(lista);
+        }
+
+
+        public IActionResult RacuniUnutrasnjiSaobracaj()
+        {
+            return View();
+        }
+    }
+}
