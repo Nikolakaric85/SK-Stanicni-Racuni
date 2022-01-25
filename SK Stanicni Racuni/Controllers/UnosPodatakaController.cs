efd34ff7dd493725af7338baa6fb09c583ec22ef
Nikola Karić <nikolakaricsd@gmail.com>
2022-01-25 15:46:38 +0100

    Forma za unos podataka K165a. Ako ima u SlogKalk pokaze onda polja za unos ako nema nista. Ogranicio polja dinamicki da moze da unosi decimale do 12.2,  ubilo kom slucaju ce imati samo 2 decimale. Forma i upit za izlistavanje K165a izvestaja. Videti sta uzeti za primary key za SlogKalk tabelu.

--- SK Stanicni Racuni/Controllers/UnosPodatakaController.cs
+++ SK Stanicni Racuni/Controllers/UnosPodatakaController.cs
@@ -2,7 +2,9 @@
 using AspNetCoreHero.ToastNotification.Abstractions;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using SK_Stanicni_Racuni.Models;
+using SK_Stanicni_Racuni.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Data;
@@ -47,72 +49,93 @@ namespace SK_Stanicni_Racuni.Controllers
             return View();
         }
 
-        public IActionResult PrintK165a(string id, string stanica, string blagajna, DateTime DatumDo)
+        public IActionResult Pretraga(string stanica, string prBroj)
         {
-            var sifraStanice = context.ZsStanices.Where(x => x.Naziv == stanica).Select(x => x.SifraStanice).FirstOrDefault(); //primer 7213670
-            var nazivStanice = context.ZsStanices.Where(x => x.Naziv == stanica).Select(x => x.Naziv).FirstOrDefault(); //primer 7213670
+            var UserId = HttpContext.User.Identity.Name; // daje UserId
+            var user = context.UserTabs.Where(x => x.UserId == UserId).FirstOrDefault();
+            var prStanica = context.ZsStanices.Where(x => x.SifraStanice1 == user.Stanica).FirstOrDefault().SifraStanice;
+            ViewBag.Stanica = stanica;
 
-            var query = from ZsStanice in context.ZsStanices
-                        join SlogKalk in context.SlogKalks
-                        on ZsStanice.SifraStanice equals SlogKalk.PrStanica
-                        where ((SlogKalk.Saobracaj == "1") || (SlogKalk.Saobracaj == "2"))
-                        && SlogKalk.K165a == 'D'
-                        && SlogKalk.Stanica == sifraStanice
-                        && SlogKalk.K165a_datum == DatumDo
-                        select new
-                        {
-                            PrStanica = SlogKalk.PrStanica,
-                            Saobracaj = SlogKalk.Saobracaj,
-                            Naziv = ZsStanice.Naziv,
-                            PrBroj = SlogKalk.PrBroj,
-                            K165a_iznos = SlogKalk.K165a_iznos,
-                            PrRbb = SlogKalk.PrRbb,
-                        };
+            var query = from sk in context.SlogKalks
+                        where (sk.Saobracaj == "1" || sk.Saobracaj == "2") &&
+                        sk.PrBroj == Int32.Parse(prBroj) &&
+                        sk.PrStanica == prStanica
 
-            var dt = new DataTable();
+                        select new { sk.K165a, sk.K165a_iznos, sk.K165a_datum };
 
-            dt.Columns.Add("PrBroj");
-            dt.Columns.Add("UnutrasnjiIznos");
-            dt.Columns.Add("UnutrasnjiIznos_pare");
-            dt.Columns.Add("MedjunarodniIznos");
-            dt.Columns.Add("MedjunarodniIznos_pare");
-            // Treba isproveravati ovo da li je ok sa parama 
-            DataRow row;
+            var viewModel = new UnosK165aViewModel();
 
-            foreach (var item in query)
+            List<string> k165aList = new List<string>() { " ", "N", "D" };
+
+            try
             {
-                row = dt.NewRow();
-                row["PrBroj"] = item.PrBroj;
+                if (query.Any())
+                {
+                    ViewBag.K165a = true;
+                    foreach (var item in query)
+                    {
 
-                var TlSuma = item.K165a_iznos.ToString();
-                string[] array = TlSuma.Split('.');
+                        if (item.K165a == '\0')
+                        {
+                            ViewBag.k165aList = new SelectList(k165aList, k165aList[0]);
+                        }
+                        else if (item.K165a == 'N')
+                        {
+                            ViewBag.k165aList = new SelectList(k165aList, k165aList[1]);
+                        }
+                        else if (item.K165a == 'D')
+                        {
+                            ViewBag.k165aList = new SelectList(k165aList, k165aList[2]);
+                        }
 
-                row["UnutrasnjiIznos"] = item.Saobracaj == "1" ? array[0] : null;
-                row["UnutrasnjiIznos_pare"] = item.Saobracaj == "1" ? array[1] : null;
-                row["MedjunarodniIznos"] = item.Saobracaj == "2" ? array[0] : null;
-                row["MedjunarodniIznos_pare"] = item.Saobracaj == "2" ? array[1] : null;
+                        viewModel.K165a_iznos = item.K165a_iznos;
+                        viewModel.K165a_datum = item.K165a_datum;
+                        viewModel.PrBroj = Int32.Parse(prBroj);
+                        viewModel.PrStanica = prStanica;
+                    }
+                }
+                else
+                {
+                    ViewBag.K165a = false;
+                }
 
-                dt.Rows.Add(row);
+            }
+            catch (Exception)
+            {
+
+                ViewBag.K165a = false;
             }
 
-            string mimtype = "";
-            int extension = 1;
-
-            Dictionary<string, string> paramtars = new Dictionary<string, string>();
-
-            paramtars.Add("SifraStanice", sifraStanice);
-            paramtars.Add("NazivStanice", nazivStanice);
-            paramtars.Add("Blagajna", blagajna);
-            paramtars.Add("DatumDo", DatumDo.ToString());
-
-            var path = $"{this.webHostEnvironment.WebRootPath}\\Reports\\K165a.rdlc";
-            LocalReport localReport = new LocalReport(path);
-            localReport.AddDataSource("K165a", dt);
-            extension = (int)(DateTime.Now.Ticks >> 10);
-            ReportResult result = localReport.Execute(RenderType.Pdf, extension, paramtars, mimtype);
-            return File(result.MainStream, "application/pdf");
-
+            return View("K165a", viewModel);
         }
+
+        public IActionResult K165a_save(UnosK165aViewModel viewModel, string k165aDatum)
+        {
+            var model = context.SlogKalks.Where(x => x.PrBroj == viewModel.PrBroj && x.PrStanica == viewModel.PrStanica).FirstOrDefault();
+
+            var newModel = new SlogKalk();
+
+            newModel = model;
+            newModel.K165a = viewModel.K165a;
+            newModel.K165a_iznos = viewModel.K165a_iznos;
+            _ = k165aDatum != null ? newModel.K165a_datum = DateTime.Parse(k165aDatum) : newModel.K165a_datum = model.K165a_datum;
+            
+            try
+            {
+                var update = context.SlogKalks.Attach(newModel);
+                update.State = Microsoft.EntityFrameworkCore.EntityState.Modified;                                      //*********************PRIMERY KEY *********************///
+                context.SaveChanges();
+                notyf.Success("Uspeša izmena podataka.",3);
+            }
+            catch (Exception)
+            {
+                notyf.Error("Greška prilikom izmene podataka.", 3);
+            }
+
+            return View("K165a");
+        }
+
+    
 
         public IActionResult K161f()
         {
@@ -121,7 +144,8 @@ namespace SK_Stanicni_Racuni.Controllers
             if (user != null)
             {
                 ViewBag.Stanica = context.ZsStanices.Where(x => x.SifraStanice1 == user.Stanica).FirstOrDefault().Naziv;
-            } else
+            }
+            else
             {
                 return RedirectToAction("Login", "Account");
             }
---
+++ SK Stanicni Racuni/ViewModels/UnosK165aViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SK_Stanicni_Racuni.ViewModels
+{
+    public class UnosK165aViewModel
+    {
+        public DateTime? K165a_datum { get; set; }
+        public char K165a { get; set; }
+        public decimal K165a_iznos { get; set; }
+        public int? PrBroj { get; set; }
+        public string PrStanica { get; set; }
+    }
+}
